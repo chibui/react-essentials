2adf40f5db8bb75cfaebbd0d44763b7975590cd1
Chi Bui <chi_bui@outlook.com>
2021-01-05 09:09:25 +1100

    Refactor usings JSX

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1", {
-    style: { color: "blue" }
-  }, "Hey Everyone!"),
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+  </ul>,
   document.getElementById('root')
 );
