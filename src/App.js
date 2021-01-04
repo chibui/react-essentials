550fd637d372855e8cd5617dfcfc8321d69a82a7
Chi Bui <chi_bui@outlook.com>
2021-01-05 10:52:32 +1100

    Working with use effect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() { // object destructuring
   // [currentState, setState]
-  const [emotion, setEmotion] = useState(1);
+  const [emotion, setEmotion] = useState('happy');
+  const [secondary, setSecondary] = useState('tired');
   
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
+
   return (
     <>
-      <h1>Current emotion is {emotion}.</h1>
-      <button onClick={ () => setEmotion('happy')}>Happy</button>
+      <h1>Current emotion is {emotion} and {secondary}.</h1>
+      <button onClick={ () => setEmotion('happy')}>Make happy</button>
+      <button onClick={ () => setEmotion('crabby')}>Make crabby</button>
       <button onClick={ () => setEmotion('frustrated')}>Frustrate</button>
       <button onClick={ () => setEmotion('enthusiastic')}>Enthuse</button>
     </>
