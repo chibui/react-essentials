e0b20fd56d8f1caf5b72ad79fe3112ca8d481aae
Chi Bui <chi_bui@outlook.com>
2021-01-06 11:18:42 +1100

    breadcrumb

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 export function Home() {
     return (
         <div>
             <h1>[Company Website]</h1>
+            <nav>
+                <Link to="/about">About</Link>
+                <Link to="/events">Events</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
         </div>
     )
 }
@@ -11,7 +17,11 @@ export function Home() {
 export function About() {
     return (
         <div>
-            <h1>[About]</h1>
+            <h1>[About]</h1>            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/events">Events</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
         </div>
     )
 }
@@ -19,7 +29,11 @@ export function About() {
 export function Events() {
     return (
         <div>
-            <h1>[Events]</h1>
+            <h1>[Events]</h1>            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
         </div>
     )
 }
@@ -27,7 +41,21 @@ export function Events() {
 export function Contact() {
     return (
         <div>
-            <h1>[Contact]</h1>
+            <h1>[Contact]</h1>            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/events">Events</Link>
+            </nav>
+        </div>
+    )
+}
+
+export function Whoops404() {
+    let location = useLocation();
+
+    return (
+        <div>
+            <h1>Resource not found at {location.pathname}!</h1>
         </div>
     )
 }
