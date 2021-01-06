bfa9d70b94f2eec9854215337cd83b0b63172300
Chi Bui <chi_bui@outlook.com>
2021-01-06 11:08:40 +1100

    Configure router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import {  Home,  About,  Events,  Contact } from './pages';
 
 function App() {
   return (
-    <div>
-      <h1>Hello React Testing Library</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/events" element={<Events />}/>
+      <Route path="/Contact" element={<Contact />}/>
+    </Routes>
   )
 }
 
