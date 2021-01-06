08c3768564a2020760db10f3ba735cc2b9525864
Chi Bui <chi_bui@outlook.com>
2021-01-06 11:27:43 +1100

    Nesting Links with react-router 6

--- src/pages.js
+++ src/pages.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, Outlet } from 'react-router-dom';
 
 export function Home() {
     return (
@@ -11,43 +11,71 @@ export function Home() {
                 <Link to="/contact">Contact</Link>
             </nav>
         </div>
-    )
+    );
 }
 
 export function About() {
     return (
         <div>
-            <h1>[About]</h1>            <nav>
+            <h1>[About]</h1>
+            <nav>
                 <Link to="/">Home</Link>
                 <Link to="/events">Events</Link>
                 <Link to="/contact">Contact</Link>
             </nav>
+            <Outlet />
         </div>
-    )
+    );
 }
 
 export function Events() {
     return (
         <div>
-            <h1>[Events]</h1>            <nav>
+            <h1>[Events]</h1>            
+            <nav>
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
             </nav>
         </div>
-    )
+    );
 }
 
 export function Contact() {
     return (
         <div>
-            <h1>[Contact]</h1>            <nav>
+            <h1>[Contact]</h1>            
+            <nav>
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/events">Events</Link>
             </nav>
         </div>
-    )
+    );
+}
+
+export function Services() {
+    return(
+        <div>
+            <h2>Our Services</h2>
+        </div>
+    );
+}
+
+export function CompanyHistory() {
+    return(
+        <div>
+            <h2>Company History</h2>
+        </div>
+    );
+}
+
+export function Location() {
+    return(
+        <div>
+            <h2>Our Location</h2>
+        </div>
+    );
 }
 
 export function Whoops404() {
@@ -57,5 +85,5 @@ export function Whoops404() {
         <div>
             <h1>Resource not found at {location.pathname}!</h1>
         </div>
-    )
+    );
 }
